cbf751b459390210c423ba1d7a811b7b1a5deb4a
Anthony-Sin <anthonysinchi655@gmail.com>
2026-05-01 23:24:57 -0400

    fixed

--- src/tui/renderer.js
+++ src/tui/renderer.js
@@ -240,8 +240,6 @@ export function Dashboard({ searchTerm = '' }) {
         seen.add(s.id)
       }
     })
-    return mapped
-  }, [sessionsData, sortedIds])
 
     // Then add the sorted items
     sortedIds.forEach(id => {
